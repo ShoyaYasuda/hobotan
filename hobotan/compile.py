b80d3f4e0edd572a71c58bb7511c14493a75f86a
YasuD <43514999+ShoyaYasuda@users.noreply.github.com>
2024-10-13 17:17:08 +0900

    Add files via upload

--- hobotan/compile.py
+++ hobotan/compile.py
@@ -1,6 +1,6 @@
 import re
-import numpy as np
 import symengine
+import numpy as np
 from sympy import Rational
 
 
@@ -17,120 +17,196 @@ def replace_function(expression, function, new_function):
         else:
             return expression.func(*replaced_args)
 
-class Compile:
-    def __init__(self, expr):
-        self.expr = expr
 
+
+class Compile:
+    def __init__(self, expr, mode='GPU', device='cuda:0', verbose=1):
+        self.expr = expr
+        self.mode = mode
+        self.device_input = device
+        self.verbose = verbose
+    
     #hoboテンソル作成
     def get_hobo(self):
-        
-        #式を展開して同類項をまとめる
-        expr = symengine.expand(self.expr)
-        
-        #二乗項を一乗項に変換
-        expr = replace_function(expr, lambda e: isinstance(e, symengine.Pow) and e.exp == 2, lambda e, *args: e)
-        
-        #最高字数を調べながらオフセットを記録
-        #項に分解
-        members = str(expr).split(' ')
-        
-        #各項をチェック
-        offset = 0
-        ho = 0
-        for member in members:
-            #数字単体ならオフセット
+        """
+        get hobo data
+        Raises:
+            TypeError: Input type is symengine.
+        Returns:
+            [hobo, index_map], offset. hobo is numpy tensor.
+        """
+        #symengine型のサブクラス
+        if 'symengine.lib' in str(type(self.expr)):
+            #式を展開して同類項をまとめる
+            expr = symengine.expand(self.expr)
+            # print('expr')
+            
+            #二乗項を一乗項に変換
+            expr = replace_function(expr, lambda e: isinstance(e, symengine.Pow) and e.exp == 2, lambda e, *args: e)
+            
+            #最高字数を調べながらオフセットを記録
+            #項に分解
+            members = str(expr).split(' ')
+            
+            #各項をチェック
+            offset = 0
+            ho = 0
+            for member in members:
+                # print(member)
+                #数字単体ならオフセット
+                try:
+                    offset += float(member) #エラーなければ数字
+                    # print('continue')
+                    continue
+                except:
+                    pass
+                #'*'で分解
+                texts = member.split('*')
+                #係数を取り除く
+                try:
+                    texts[0] = re.sub(r'[()]', '', texts[0]) #'(5/2)'みたいなのも来る
+                    # print(texts[0])
+                    float(Rational(texts[0])) #分数も対応 #エラーなければ係数あり
+                    texts = texts[1:]
+                except:
+                    # print('err')
+                    pass
+                
+                #最高次数の計算
+                # ['-']
+                # ['q2']
+                # ['q3', 'q4', 'q1', 'q2']
+                if len(texts) > ho:
+                    ho = len(texts)
+            # print(ho)
+            
+            # #もう一度同類項をまとめる
+            # expr = symengine.expand(expr)
+            # print('expr')
+    
+            #文字と係数の辞書
+            coeff_dict = expr.as_coefficients_dict()
+            # print(len(coeff_dict))
+            
+            #定数項を消す　{1: 25} 必ずある
+            del coeff_dict[1]
+            # print('del')
+            # print(coeff_dict)
+            
+            #シンボル対応表
+            # 重複なしにシンボルを抽出
+            tmp = []
+            # count = 0
+            for key in coeff_dict.keys():
+                # if count % 1000 == 0:
+                #     print(count)
+                # count += 1
+                tmp += str(key).split('*')
+            # tmp = sum([str(key).split('*') for key in coeff_dict.keys()], [])
+            # print(tmp)
+            # print('tmp')
+            keys = list(set(tmp))
+            # print(len(keys))
+            
+            # 要素のソート（ただしアルファベットソート）
+            keys.sort()
+            # print(keys)
+            
+            # シンボルにindexを対応させる
+            index_map = {key:i for i, key in enumerate(keys)}
+            # print(index_map)
+            
+            #量子ビット数
+            num = len(index_map)
+            # print(num)
+            
+            #pytorch確認
+            attention = False
             try:
-                offset += float(member) #エラーなければ数字
+                import random
+                import torch
             except:
-                pass
-            #'*'で分解
-            texts = member.split('*')
-            #係数を取り除く
-            try:
-                texts[0] = re.sub(r'[()]', '', texts[0]) #'(5/2)'みたいなのも来る
-                float(Rational(texts[0])) #分数も対応 #エラーなければ係数あり
-                texts = texts[1:]
-            except:
-                pass
+                attention = True
+            if attention:
+                print()
+                print('=======================\n= A T T E N T I O N ! =\n=======================')
+                print('ArminSampler requires PyTorch installation.\nUse "pip install torch" (or others) and ensure\n-------\nimport torch\ntorch.cuda.is_available()\n#torch.backends.mps.is_available() #if Mac\n-------\noutputs True to set up the environment.')
+                print()
+                sys.exit()
             
-            # 以下はセーフ
-            # q0   ['q0']
-            # q0*q1   ['q0', 'q1']
-            # q0**2   ['q0', '', '2']
+            # CUDA使えるか確認
+            if self.mode == 'GPU' and torch.cuda.is_available():
+                if self.device_input == 'cuda:0': #指示がない場合
+                    self.device = 'cuda:0'
+                else:
+                    self.device = self.device_input
+            elif self.mode == 'GPU' and torch.backends.mps.is_available():
+                if self.device_input == 'cuda:0': #指示がない場合
+                    self.device = 'mps:0'
+                else:
+                    self.device = self.device_input
+            else:
+                self.mode = 'CPU'
+                self.device = 'cpu'
             
-            # 以下はダメ
-            # q0*q1**2   ['q0', 'q1', '', '2']
-            # q0*q1*q2   [q0', 'q1', 'q2']
-            # q0**2*q1**2    ['q0', '', '2', 'q1', '', '2']
-            # if len(texts) >= 4:
-            #     raise Exception(f'Error! The highest order of the constraint must be within 2.')
-            # if len(texts) == 3 and texts[1] != '':
-            #     raise Exception(f'Error! The highest order of the constraint must be within 2.')
+            #モード表示
+            if self.verbose > 0:
+                print('----- compile -----')
+                print(f'MODE: {self.mode}')
+                print(f'DEVICE: {self.device}')
+                print('-------------------')
             
-            #最高次数の計算
-            # ['-']
-            # ['q2']
-            # ['q3', 'q4', 'q1', 'q2']
-            if len(texts) > ho:
-                ho = len(texts)
-        # print(ho)
-        
-        #もう一度同類項をまとめる
-        expr = symengine.expand(expr)
+            #HOBO行列生成
+            
+            # インデックスと値を格納
+            indices_all = []
+            value_all = []
+            for key, value in coeff_dict.items():
+                qnames = str(key).split('*')
+                indices = sorted([index_map[qname] for qname in qnames])
+                indices = [indices[0]] * (ho - len(indices)) + indices
+                indices_all.append(indices)
+                value_all.append(float(value))
+            indices_all = np.array(indices_all).T
+            value_all = np.array(value_all)
+            # print(indices_all.shape)
+            # print(value_all.shape)
+            # print(np.min(indices_all), np.max(indices_all))
+            # print(np.min(value_all), np.max(value_all))
+            
+            # インデックスと値
+            indices = torch.tensor(indices_all, dtype=torch.int, device=self.device)
+            values = torch.tensor(value_all, dtype=torch.float32, device=self.device)
+            
+            # 疎テンソル自体は作成せず、インデックスと値を格納する
+            hobo = [indices, values, num]
+            
+            # # 疎テンソルを作成
+            # print(num)
+            # print(ho)
+            
+            # if ho >= 8:
+            #     hobo1 = torch.sparse_coo_tensor(indices_all[:6], value_all, [num] * 6, dtype=torch.float32, device=self.device)
+            #     hobo2 = torch.sparse_coo_tensor(indices_all[6:], value_all, [num] * (ho - 6), dtype=torch.float32, device=self.device)
+            #     hobo = [hobo1, hobo2]
+            # else:
+            #     hobo = torch.sparse_coo_tensor(indices_all, value_all, [num] * ho, dtype=torch.float32, device=self.device)
+            # # print(hobo.shape)
+            
+            
+            # hobo = hobo.to_dense()
+            # print(hobo.dtype)
+            
+            # hobo = np.zeros(num ** ho, dtype=float)
+            # hobo = hobo.reshape([num] * ho)
+            # for key, value in coeff_dict.items():
+            #     qnames = str(key).split('*')
+            #     indices = sorted([index_map[qname] for qname in qnames])
+            #     indices = [indices[0]] * (ho - len(indices)) + indices
+            #     hobo[tuple(indices)] = float(value)
+            
+            return [hobo, index_map], offset
 
-        #文字と係数の辞書
-        coeff_dict = expr.as_coefficients_dict()
-        # print(coeff_dict)
-        
-        #定数項を消す　{1: 25} 必ずある
-        del coeff_dict[1]
-        # print(coeff_dict)
-        
-        #シンボル対応表
-        # 重複なしにシンボルを抽出
-        keys = list(set(sum([str(key).split('*') for key in coeff_dict.keys()], [])))
-        # print(keys)
-        
-        # 要素のソート（ただしアルファベットソート）
-        keys.sort()
-        # print(keys)
-        
-        # シンボルにindexを対応させる
-        index_map = {key:i for i, key in enumerate(keys)}
-        # print(index_map)
-        
-        #量子ビット数
-        num = len(index_map)
-        # print(num)
-        
-        """
-        #旧実装
-        #HOBO行列生成コマンド
-        command = 'global hobo\r\n'
-        command += f'if ho == {ho}:\r\n'
-        command += f'    hobo = np.zeros([{num}' + f', {num}' * (ho - 1) + '])\r\n'
-        command += f'    for key, value in coeff_dict.items():\r\n'
-        command += f'        tmp = str(key).split(\'*\')\r\n'
-        for i in range(1, ho + 1):
-            command += f'        if len(tmp) == {i}:\r\n'
-            command += f'            hobo[index_map[tmp[0]]'
-            j = i - (ho - 1)
-            for _ in range(ho - 1):
-                command += f', index_map[tmp[{max(0, j)}]]'
-                j += 1
-            command += f'] = float(value)\r\n'
-        print(command)
-        
-        #HOBO生成
-        exec(command)
-        """
-        
-        #HOBO行列生成
-        hobo = np.zeros(num ** ho, dtype=float).reshape([num] * ho)
-        for key, value in coeff_dict.items():
-            qnames = str(key).split('*')
-            indices = sorted([index_map[qname] for qname in qnames])
-            indices = [indices[0]] * (ho - len(indices)) + indices
-            hobo[tuple(indices)] = float(value)
-        
-        return [hobo, index_map], offset
+        else:
+            raise TypeError("Input type must be symengine.")
+
--- hobotan/sampler.py
+++ hobotan/sampler.py
@@ -2,7 +2,7 @@ import sys
 import time
 import numpy as np
 import numpy.random as nr
-
+from copy import deepcopy
 
 #共通後処理
 """
@@ -33,172 +33,6 @@ def get_result(pool, score, index_map):
 #謎のglobal対応
 score2 = 0
 
-#アニーリング
-class SASampler:
-    def __init__(self, seed=None):
-        #乱数シード
-        self.seed = seed
-
-    
-    def run(self, hobomix, shots=100, T_num=2000, show=False):
-        global score2
-        
-        #解除
-        hobo, index_map = hobomix
-        # print(index_map)
-        
-        #matrixサイズ
-        N = len(hobo)
-        # print(N)
-        
-        #次数
-        ho = len(hobo.shape)
-        # print(ho)
-        
-        #シード固定
-        nr.seed(self.seed)
-        
-        #
-        shots = max(int(shots), 100)
-        
-        # プール初期化
-        pool_num = shots
-        pool = nr.randint(0, 2, (pool_num, N)).astype(float)
-        # print(pool)
-        
-        """
-        poolの重複を解除する
-        """
-        # 重複は振り直し
-        # パリエーションに余裕あれば確定非重複
-        if pool_num < 2 ** (N - 1):
-            # print('remake 1')
-            for i in range(pool_num - 1):
-                for j in range(i + 1, pool_num):
-                    while (pool[i] == pool[j]).all():
-                        pool[j] = nr.randint(0, 2, N)
-        else:
-            # パリエーションに余裕なければ3トライ重複可
-            # print('remake 2')
-            for i in range(pool_num - 1):
-                for j in range(i + 1, pool_num):
-                    count = 0
-                    while (pool[i] == pool[j]).all():
-                        pool[j] = nr.randint(0, 2, N)
-                        count += 1
-                        if count == 3:
-                            break
-        
-        #スコア初期化
-        score = np.zeros(pool_num)
-        
-        """
-        #旧実装
-        # einsum("ijk,i,j,k->", qmatrix, x, x, x)
-        # einsum("ijk,Ni,Nj,Nk->N", qmatrix, pool, pool, pool)
-        
-        #スコア計算コマンド
-        k = ',Na,Nb,Nc,Nd,Ne,Nf,Ng,Nh,Nj,Nk,Nl,Nm,Nn,No,Np,Nq,Nr,Ns,Nt,Nu,Nv,Nw,Nx,Ny,Nz'
-        l = 'abcdefghjklmnopqrstuvwxyz'
-        s = l[:ho] + k[:3*ho] + '->N'
-        # print(s)
-        command = 'global score2\r\n'
-        command += f'score2 = np.zeros(pool_num)\r\n'
-        command += f'score2 = np.einsum(\'{s}\', hobo, pool2' + ', pool2' * (ho - 1) + f')\r\n'
-        # print(command)
-        
-        #スコア計算
-        pool2 = pool
-        exec(command)
-        score = score2
-        # print(score)
-        """
-        
-        #スコア計算
-        k = ',Na,Nb,Nc,Nd,Ne,Nf,Ng,Nh,Nj,Nk,Nl,Nm,Nn,No,Np,Nq,Nr,Ns,Nt,Nu,Nv,Nw,Nx,Ny,Nz'
-        l = 'abcdefghjklmnopqrstuvwxyz'
-        s = l[:ho] + k[:3*ho] + '->N'
-        # print(s)
-        
-        operands = [hobo] + [pool] * ho
-        score = np.einsum(s, *operands)
-        # print(score)
-        
-        # フリップ数リスト（2個まで下がる）
-        flip = np.sort(nr.rand(T_num) ** 2)[::-1]
-        flip = (flip * max(0, N * 0.5 - 2)).astype(int) + 2
-        # print(flip)
-        
-        # フリップマスクリスト
-        flip_mask = [[1] * flip[0] + [0] * (N - flip[0])]
-        if N <= 2:
-            flip_mask = np.ones((T_num, N), int)
-        else:
-            for i in range(1, T_num):
-                tmp = [1] * flip[i] + [0] * (N - flip[i])
-                nr.shuffle(tmp)
-                # 前と重複なら振り直し
-                while tmp == flip_mask[-1]:
-                    nr.shuffle(tmp)
-                flip_mask.append(tmp)
-            flip_mask = np.array(flip_mask, bool)
-        # print(flip_mask.shape)
-        
-        # 局所探索フリップマスクリスト
-        single_flip_mask = np.eye(N, dtype=bool)
-        
-        """
-        アニーリング＋1フリップ
-        """
-        # アニーリング
-        # 集団まるごと温度を下げる
-        for fm in flip_mask:
-            # フリップ後　pool_num, N
-            # pool2 = np.where(fm, 1 - pool, pool)
-            pool2 = pool.copy()
-            pool2[:, fm] = 1. - pool[:, fm]
-            # score2 = np.sum((pool2 @ qmatrix) * pool2, axis=1)
-            
-            operands = [hobo] + [pool2] * ho
-            score2 = np.einsum(s, *operands)
-            
-            # 更新マスク
-            update_mask = score2 < score
-            # print(update_mask)
-    
-            # 更新
-            pool[update_mask] = pool2[update_mask]
-            score[update_mask] = score2[update_mask]
-        
-        # 最後に1フリップ局所探索
-        # 集団まるごと
-        for fm in single_flip_mask:
-            # フリップ後
-            # pool2 = np.where(fm, 1 - pool, pool)
-            pool2 = pool.copy()
-            pool2[:, fm] = 1. - pool[:, fm]
-            # score2 = np.sum((pool2 @ qmatrix) * pool2, axis=1)
-            
-            operands = [hobo] + [pool2] * ho
-            score2 = np.einsum(s, *operands)
-            
-            # 更新マスク
-            update_mask = score2 < score
-            # print(update_mask)
-    
-            # 更新
-            pool[update_mask] = pool2[update_mask]
-            score[update_mask] = score2[update_mask]
-        pool = pool.astype(int)
-        
-        # ----------
-        #共通後処理
-        result = get_result(pool, score, index_map)
-        
-        return result
-
-
-
 class MIKASAmpler:
     def __init__(self, seed=None, mode='GPU', device='cuda:0', verbose=1):
         #乱数シード
@@ -207,7 +41,7 @@ class MIKASAmpler:
         self.device_input = device
         self.verbose = verbose
 
-    def run(self, hobomix, shots=100, T_num=2000, use_ttd=False, show=False):
+    def run(self, hobomix, shots=100, T_num=2000, show=False):
         global score2
         
         #解除
@@ -228,14 +62,6 @@ class MIKASAmpler:
             print()
             sys.exit()
         
-        #matrixサイズ
-        N = len(hobo)
-        # print(N)
-        
-        #次数
-        ho = len(hobo.shape)
-        # print(ho)
-        
         # CUDA使えるか確認
         if self.mode == 'GPU' and torch.cuda.is_available():
             if self.device_input == 'cuda:0': #指示がない場合
@@ -253,8 +79,10 @@ class MIKASAmpler:
         
         #モード表示
         if self.verbose > 0:
+            print('===== sampler =====')
             print(f'MODE: {self.mode}')
             print(f'DEVICE: {self.device}')
+            print('===================')
     
         # ランダムシード
         random.seed(int(time.time()))
@@ -272,19 +100,29 @@ class MIKASAmpler:
         #
         shots = max(int(shots), 100)
         
+        
+        
+        # インデックスと値復元
+        indices, values, N = hobo
+        # hobo_coalesced = hobo.coalesce()
+        # indices = hobo_coalesced.indices()  # インデックスを取得
+        # values = hobo_coalesced.values()    # 値を取得
+        # print(indices)
+        # print(values)
+        
+        #matrixサイズ
+        # N = len(indices[0])
+        # print(N)
+        
+        #次数
+        ho = len(indices)
+        # print(ho)
+        
+        
         # --- テンソル疑似SA ---
         #
-        hobo = torch.tensor(hobo, dtype=torch.float32, device=self.device).float()
+        # hobo = torch.tensor(hobo, dtype=torch.float32, device=self.device).float()
         # print(hobo.shape)
-        
-        #TT分解を使用する場合
-        tt_cores = []
-        if use_ttd:
-            print(f'TTD: {use_ttd}')
-            tt_cores = TT_SVD(hobo)
-            # print(len(tt_cores))
-            # print(tt_cores[0].shape)
-            # print(tt_cores[1].shape)
         
         # プール初期化
         pool_num = shots
@@ -293,47 +131,13 @@ class MIKASAmpler:
         # スコア初期化
         # score = torch.sum((pool @ qmatrix) * pool, dim=1, dtype=torch.float32)
         score = torch.zeros(pool_num, dtype=torch.float32)
+        values2 = torch.clone(values)
+        for i in range(ho):
+            values2 = pool[:, indices[i]] * values2   # a 次元の para を掛ける
+            # print(values2)
+        score = torch.sum(values2, axis=1)
         # print(score)
         
-        """
-        #旧実装
-        # einsum("ijk,i,j,k->", hobo, x, x, x)
-        # einsum("ijk,Ni,Nj,Nk->N", hobo, pool, pool, pool)
-        
-        #スコア計算コマンド
-        k = ',Na,Nb,Nc,Nd,Ne,Nf,Ng,Nh,Nj,Nk,Nl,Nm,Nn,No,Np,Nq,Nr,Ns,Nt,Nu,Nv,Nw,Nx,Ny,Nz'
-        l = 'abcdefghjklmnopqrstuvwxyz'
-        s = l[:ho] + k[:3*ho] + '->N'
-        # print(s)
-        command = 'global score2\r\n'
-        command += f'score2 = torch.zeros(pool_num, dtype=torch.float32)\r\n'
-        command += f'score2 = torch.einsum(\'{s}\', hobo, pool2' + ', pool2' * (ho - 1) + f')\r\n'
-        print(command)
-        
-        #スコア計算
-        pool2 = pool
-        exec(command)
-        score = score2
-        print(score)
-        """
-        
-        #スコア計算
-        k = ',Na,Nb,Nc,Nd,Ne,Nf,Ng,Nh,Nj,Nk,Nl,Nm,Nn,No,Np,Nq,Nr,Ns,Nt,Nu,Nv,Nw,Nx,Ny,Nz'
-        l = 'abcdefghjklmnopqrstuvwxyz'
-        if use_ttd:
-            ltt = ['aA', 'AbB', 'BcC', 'CdD', 'DeE', 'EfF', 'FgG', 'GhH', 'HiJ', 'JjK', 'KkL', 'LlM', 'MmO', 'OnP', 'PoQ', 'QpR', 'RqS', 'SrT', 'TsU', 'UuV', 'VvW', 'WwX', 'XxY', 'YyZ', 'Zz']
-            ltt = ltt[:ho][:]
-            if len(ltt[-1]) == 3:
-                ltt[-1] = ltt[-1][:2]  # 両端は 2 階のテンソルなので 2 つのインデックスのみ
-            s = ','.join(ltt) + k[:3*ho] + '->N'
-            operands = tt_cores + [pool] * ho
-        else:
-            s = l[:ho] + k[:3*ho] + '->N'
-            operands = [hobo] + [pool] * ho
-        # print(s)
-        
-        score = torch.einsum(s, *operands)
-        # print(score)
         
         # フリップ数リスト（2個まで下がる）
         flip = np.sort(nr.rand(T_num) ** 2)[::-1]
@@ -368,16 +172,24 @@ class MIKASAmpler:
         """
         # アニーリング
         # 集団まるごと温度を下げる
+        count = 1
         for fm in flip_mask:
+            
+            if count % 10 == 0:
+                print('.', end='')
+                if count % 100 == 0:
+                    print(f' {count}/{T_num}')
+            count += 1
+            
             pool2 = pool.clone()
             pool2[:, fm] = 1. - pool[:, fm]
             # score2 = torch.sum((pool2 @ qmatrix) * pool2, dim=1)
-    
-            if use_ttd:
-                operands = tt_cores + [pool2] * ho
-            else:
-                operands = [hobo] + [pool2] * ho
-            score2 = torch.einsum(s, *operands)
+            
+            # スコア
+            values2 = torch.clone(values)
+            for i in range(ho):
+                values2 = pool2[:, indices[i]] * values2
+            score2 = torch.sum(values2, axis=1)
     
             # 更新マスク
             update_mask = score2 < score
@@ -388,7 +200,7 @@ class MIKASAmpler:
             
             # スコア記録
             score_history.append(torch.mean(score).item())
-            
+        
         # 最後に1フリップ局所探索
         # 集団まるごと
         for fm in single_flip_mask:
@@ -396,11 +208,11 @@ class MIKASAmpler:
             pool2[:, fm] = 1. - pool[:, fm]
             # score2 = torch.sum((pool2 @ qmatrix) * pool2, dim=1)
     
-            if use_ttd:
-                operands = tt_cores + [pool2] * ho
-            else:
-                operands = [hobo] + [pool2] * ho
-            score2 = torch.einsum(s, *operands)
+            # スコア
+            values2 = torch.clone(values)
+            for i in range(ho):
+                values2 = pool2[:, indices[i]] * values2
+            score2 = torch.sum(values2, axis=1)
     
             # 更新マスク
             update_mask = score2 < score
@@ -421,6 +233,7 @@ class MIKASAmpler:
             plt.show()
         
         pool = pool.to('cpu').detach().numpy().copy()
+        pool = pool.astype(int)
         score = score.to('cpu').detach().numpy().copy()
         
         # ----------
@@ -429,69 +242,9 @@ class MIKASAmpler:
         
         return result
 
-def TT_SVD(C, bond_dims=None, check_bond_dims=False, return_sv=False):
-    """TT_SVD algorithm
-    I. V. Oseledets, Tensor-Train Decomposition, https://epubs.siam.org/doi/10.1137/090752286, Vol. 33, Iss. 5 (2011)
-    Args:
-        C (torch.Tensor): n-dimensional input tensor
-        bond_dims (Sequence[int]): a list of bond dimensions.
-                                   If `bond_dims` is None,
-                                   `bond_dims` will be automatically calculated
-        check_bond_dims (bool): check if `bond_dims` is valid
-        return_sv (bool): return singular values
-    Returns:
-        list[torch.Tensor]: a list of core tensors of TT-decomposition
-    """
-    import torch
 
-    dims = C.shape
-    n = len(dims)  # n-dimensional tensor
 
-    if bond_dims is None or check_bond_dims:
-        # Theorem 2.1
-        bond_dims_ = []
-        for sep in range(1, n):
-            row_dim = dims[:sep].numel()
-            col_dim = dims[sep:].numel()
-            rank = torch.linalg.matrix_rank(C.reshape(row_dim, col_dim))
-            bond_dims_.append(rank)
-        if bond_dims is None:
-            bond_dims = bond_dims_
 
-    if len(bond_dims) != n - 1:
-        raise ValueError(f"{len(bond_dims)=} must be {n - 1}.")
-    if check_bond_dims:
-        for i, (dim1, dim2) in enumerate(zip(bond_dims, bond_dims_, strict=True)):
-            if dim1 > dim2:
-                raise ValueError(f"{i}th dim {dim1} must not be larger than {dim2}.")
-
-    tt_cores = []
-    SVs = []
-    for i in range(n - 1):
-        if i == 0:
-            ri_1 = 1
-        else:
-            ri_1 = bond_dims[i - 1]
-        ri = bond_dims[i]
-        C = C.reshape(ri_1 * dims[i], dims[i + 1 :].numel())
-        U, S, Vh = torch.linalg.svd(C, full_matrices=False)
-        if S.shape[0] < ri:
-            # already size of S is less than requested bond_dims, so update the dimension
-            ri = S.shape[0]
-            bond_dims[i] = ri
-        # approximation
-        U = U[:, :ri]
-        S = S[:ri]
-        if return_sv:
-            SVs.append(S.detach().clone())
-        Vh = Vh[:ri, :]
-        tt_cores.append(U.reshape(ri_1, dims[i], ri))
-        C = torch.diag(S) @ Vh
-    tt_cores.append(C)
-    tt_cores[0] = tt_cores[0].reshape(dims[0], bond_dims[0])
-    if return_sv:
-        return tt_cores, SVs
-    return tt_cores
 
 
 if __name__ == "__main__":
